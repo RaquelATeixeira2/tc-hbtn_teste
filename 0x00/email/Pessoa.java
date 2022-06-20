6afea20431faef922496824bd8086fb3f3f15ba8
Raquel Teixeira <raquelat@ciandt.com>
2022-06-20 12:12:21 -0300

    Solid

--- 0x00/email/Pessoa.java
+++ 0x00/email/Pessoa.java
@@ -1,6 +1,6 @@
 public class Pessoa {
     public static boolean emailValid(String email){
-        if(email.length() < 50 && email.contains("@")){
+        if(email.length() < 50 || email.contains("@")){
             return true;
         }
         return false;
