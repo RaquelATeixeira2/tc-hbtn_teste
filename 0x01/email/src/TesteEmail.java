2c9e17dd362e64eb75746cbf17d353f38e43180e
Raquel Teixeira <raquelat@ciandt.com>
2022-06-20 19:55:31 -0300

    teste

--- 0x01/email/src/TesteEmail.java
+++ 0x01/email/src/TesteEmail.java
@@ -1,17 +1,19 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class TesteEmail {
+
     @Test
-    public void testar_email_com_arroba(){
+    public void testar_email_com_arroba() {
         assertTrue(Pessoa.emailValid("email_teste@dominio.com.br"));
     }
 
-    public void testar_email_sem_arroba(){
+    @Test
+    public void testar_email_sem_arroba() {
         assertFalse(Pessoa.emailValid("email_testedominio.com.br"));
     }
 
-    public static boolean testar_email_mais_50_caracteres(){
-        assertEquals(Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br"));
+    @Test
+    public void testar_email_mais_50_caracteres() {
+        assertEquals(Pessoa.emailValid("emailtestemuitolongonaodeveser_valido@dominio.com.br"), false);
     }
-
 }
